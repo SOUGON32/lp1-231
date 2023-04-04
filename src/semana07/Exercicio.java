2cbbe9556d96b73a61e5b663e2b601ae604df5db
gabriel <gsa2307@gmail.com>
2023-04-04 15:27:46 -0300

    Exerciciossemana07

---
+++ src/semana07/Exercicio.java
@@ -0,0 +1,13 @@
+package semana07;
+
+public class Exercicio {
+    public static void main (String[] args) {
+   int resultado = ExerciciosRevisao.menor(1, 3, 5);
+    System.out.println(resultado);
+    }
+    public static void main (String[] args) {
+        double[] resultado = ExerciciosRevisao.menor(1, 3, 5);
+         System.out.println(resultado);
+         }
+
+ }
---
+++ src/semana07/ExerciciosRevisao.java
@@ -0,0 +1,37 @@
+package semana07;
+
+public class ExerciciosRevisao {
+ public static Double somar (double valor1, double valor2, double valor3) {
+ 
+ return valor1 + valor2 + valor3;
+    }
+    public static Double media (double valor1, double valor2, double valor3) {
+ 
+        return (valor1 + valor2 + valor3)/3;
+           }
+           public static int maior (int valor1, int valor2, int valor3) {
+             int maior = valor1;
+             if (valor2 >maior){
+                maior= valor2;
+             }
+             if (valor3 >maior){
+                maior= valor3;
+             }
+             return maior;
+        }
+        public static int menor (int valor1, int valor2, int valor3) {
+            int menor = valor1;
+            if (valor2 < menor ){
+               menor = valor2;
+            }
+            if (valor3 < menor){
+               menor = valor3;
+            }
+            return menor;
+       }
+       public static  menor (int valor1, int valor2, int valor3) {
+        int menor = valor1;
+
+       }
+}
+ 
